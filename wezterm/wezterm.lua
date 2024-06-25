57957264357765fc2646af2ab70671585e30c932
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-06-25 16:31:59 +1000

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,8 +3,13 @@ local config = wezterm.config_builder()
 config.enable_wayland = true
 config.enable_tab_bar = false
 config.window_background_opacity = 0.6
-config.default_cursor_style = 'SteadyUnderline'
-config.color_scheme = 'tokyonight'
-config.font = wezterm.font 'CaskaydiaCove NFM'
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.window_close_confirmation = "NeverPrompt"
+config.default_cursor_style = "BlinkingUnderline"
+config.cursor_blink_rate = 700
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.color_scheme = "tokyonight"
+config.font = wezterm.font("CaskaydiaCove NFM")
 config.font_size = 12.0
 return config
