95a57d5ce8d4c2105a2295cddc1171285d919e6f
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-10-23 14:48:11 +1100

    add scripts

---
+++ scripts/increasevol.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pactl set-sink-mute @DEFAULT_SINK@ 0
+if [ $(($(pamixer --get-volume) + 5)) -ge 120 ]; then
+	pactl set-sink-volume @DEFAULT_SINK@ 120%
+else
+	pactl set-sink-volume @DEFAULT_SINK@ +5%
+fi
