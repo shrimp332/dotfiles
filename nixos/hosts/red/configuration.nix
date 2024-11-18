894970d7e11ba76ce9e87653e4af1366f8ce6b46
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-11-18 16:31:05 +1100

    update red

--- nixos/hosts/red/configuration.nix
+++ nixos/hosts/red/configuration.nix
@@ -2,7 +2,11 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
+<<<<<<< Updated upstream
 { lib, ... }:
+=======
+{ ... }:
+>>>>>>> Stashed changes
 
 {
   imports = [
@@ -11,9 +15,23 @@
 
   
   networking.hostName = "red";
+<<<<<<< Updated upstream
   networking.firewall.enable = lib.mkForce false;
 
   btrfs.enable = true;
+=======
+
+  common.enable = true;
+
+  sway.enable = true;
+
+  bluetooth.enable = true;
+  btrfs.enable = true;
+  docker.enable = true;
+  smb.enable = true;
+  vm.enable = true;
+  windows.enable = true;
+>>>>>>> Stashed changes
 
   system.stateVersion = "24.05"; # Don't change after install
 }
