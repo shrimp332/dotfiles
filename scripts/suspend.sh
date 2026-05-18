179d4245e97f784f34f6992f58f7fa3649bcef55
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2026-05-18 20:32:03 +1000

    scripts: remove old

--- scripts/suspend.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-swayidle -w before-sleep 'playerctl pause' \
-	before-sleep 'pactl set-sink-mute @DEFAULT_SINK@ 1' \
-	before-sleep 'brightnessctl set 40%' \
-	timeout 600 'systemctl suspend'
